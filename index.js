ad923caa0ef4dec033b21f19f31e40d0654d3d63
Nicat <59221034+Nicat-dcw@users.noreply.github.com>
2023-02-08 16:04:14 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const { Client } = require("discord.js")
 const client = new Client({intents:32767})
 const axios = require("axios")
-const config = { kanalId:"" }
+const config = { kanalId:"", token:"" }
 async function getLatest(){
 let d = await axios.get("https://nicat-dcw.me/api/v1/deprem")
 return d;
 }
 client.on("ready", () => {
 const call = getLatest()
-
+call.map(x=>{
+client.channels.cache.get(config.kanalId).send({
+content:`Derinlik ${x.ml} | Şehir: ${x.yer}`
+})
+})
 }
+client.login(config.token)
